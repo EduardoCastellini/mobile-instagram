1096616e716af85746769c651372f2f7789d64db
Eduardo Castellini <eduardo.castellini@hotmail.com>
2019-06-22 12:26:45 -0300

    Ajustes Finais no App

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import io from "socket.io-client";
 import api from "../services/api";
 import {
   View,
@@ -32,12 +33,29 @@ export default class Feed extends Component {
   };
 
   async componentDidMount() {
-    //his.registerToSocket();
+    this.registerToSocket();
 
     const response = await api.get("posts");
 
     this.setState({ feed: response.data });
   }
+  registerToSocket = () => {
+    const socket = io("http://10.0.3.2:3333");
+    socket.on("post", newPost => {
+      this.setState({ feed: [newPost, ...this.state.feed] });
+    });
+    socket.on("like", likedPost => {
+      this.setState({
+        feed: this.state.feed.map(post =>
+          post._id == likedPost._id ? likedPost : post
+        )
+      });
+    });
+  };
+
+  handleLike = id => {
+    api.post(`/posts/${id}/like`);
+  };
 
   render() {
     return (
@@ -60,13 +78,16 @@ export default class Feed extends Component {
               />
               <View style={styles.feedItemFooter}>
                 <View style={styles.actions}>
-                  <TouchableOpacity onPress={() => {}}>
+                  <TouchableOpacity
+                    style={styles.action}
+                    onPress={() => this.handleLike(item._id)}
+                  >
                     <Image source={like} />
                   </TouchableOpacity>
-                  <TouchableOpacity onPress={() => {}}>
+                  <TouchableOpacity style={styles.action} onPress={() => {}}>
                     <Image source={comment} />
                   </TouchableOpacity>
-                  <TouchableOpacity onPress={() => {}}>
+                  <TouchableOpacity style={styles.action} onPress={() => {}}>
                     <Image source={send} />
                   </TouchableOpacity>
                 </View>
@@ -92,7 +113,6 @@ const styles = StyleSheet.create({
   },
   feedItemHearder: {
     paddingHorizontal: 15,
-
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center"
@@ -113,5 +133,32 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 400,
     marginVertical: 15
+  },
+
+  feedItemFooter: {
+    paddingHorizontal: 15
+  },
+
+  actions: {
+    flexDirection: "row"
+  },
+
+  action: {
+    marginRight: 8
+  },
+
+  likes: {
+    marginTop: 15,
+    fontWeight: "bold",
+    color: "#000"
+  },
+
+  description: {
+    lineHeight: 18,
+    color: "#000"
+  },
+
+  hashtags: {
+    color: "#7159c1"
   }
 });
